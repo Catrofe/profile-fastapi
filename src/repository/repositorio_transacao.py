ebd8272bbeab87b3632ad10e27f5d93385edc293
Christian Lopes <christian.lima@ntendencia.com.br>
2024-06-06 20:22:31 -0300

    FEAT: Finalizando endpoint simples para demonstração do pyinstrument

---
+++ src/repository/repositorio_transacao.py
@@ -0,0 +1,28 @@
+from typing import List
+
+from infra.repositorio.entity import Transacao
+from infra.repositorio.postgres_conexao import PostgresConexao
+from models.transaction_model import TransacaoSimples
+
+
+class TransacaoRepositorio:
+    def __init__(self) -> None:
+        self.conexao = PostgresConexao()
+
+    async def registrar_transacoes(
+        self, transacoes: List[TransacaoSimples]
+    ) -> List[Transacao]:
+        transacoes_parseadas: List[Transacao] = [
+            await self.parsea_para_entidade(transacao) for transacao in transacoes
+        ]
+        session_maker = await self.conexao.busca_session_maker()
+        async with session_maker() as session:
+            async with session.begin():
+                session.add_all(transacoes_parseadas)
+                await session.commit()
+        return transacoes_parseadas
+
+    async def parsea_para_entidade(self, transacao: TransacaoSimples) -> Transacao:
+        return Transacao(
+            ativo=transacao.ativo, valor=transacao.valor, data=transacao.data
+        )
--- src/utils/models_utils.py
+++ src/utils/models_utils.py
@@ -1,4 +1,3 @@
-import uuid
 from datetime import datetime
 from zoneinfo import ZoneInfo
 
@@ -7,7 +6,3 @@ timezone = ZoneInfo("America/Sao_Paulo")
 
 def obter_data_atual() -> datetime:
     return datetime.now(tz=timezone).replace(tzinfo=None)
-
-
-def obter_uuid() -> str:
-    return str(uuid.uuid4())
